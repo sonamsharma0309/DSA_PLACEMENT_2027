b0a84962ac05cc22b5a58b74f52752bd359e3d4a
Sonam Sharma <sonamsh9000@gmail.com>
2026-09-08 09:33:52 +0530

    Time: 2 ms (91.78%), Space: 47.7 MB (76.08%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -5,8 +5,8 @@ class Solution {
         for(int i = 0;i<n;i++){
             if(nums[i]!=0){
                 int temp = nums[i];
-                nums[i] =nums[j];
-                nums[j]= temp;
+                nums[i]=nums[j];
+                nums[j]=temp;
                 j++;
             }
         }
